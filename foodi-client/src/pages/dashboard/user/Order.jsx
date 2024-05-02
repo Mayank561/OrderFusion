71a87b37d787d4f2d81edfe8827f84a76ad25300
Mayank Gupta <108197241+Mayank561@users.noreply.github.com>
2024-05-02 18:11:37 +0530

    Update Order.jsx

--- foodi-client/src/pages/dashboard/user/Order.jsx
+++ foodi-client/src/pages/dashboard/user/Order.jsx
@@ -11,7 +11,7 @@ const Order = () => {
     enabled: !loading,
     queryFn: async () => {
       const res = await fetch(
-        `http://localhost:5000/payments?email=${user?.email}`,
+        `https://foodi-2.onrender.com/payments?email=${user?.email}`,
         {
           headers: {
             authorization: `Bearer ${token}`,
