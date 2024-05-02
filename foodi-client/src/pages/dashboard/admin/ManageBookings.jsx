bb87bcbb408add9b68394946ec39f6b88804e2c5
Mayank Gupta <108197241+Mayank561@users.noreply.github.com>
2024-05-02 18:04:58 +0530

    Update ManageBookings.jsx

--- foodi-client/src/pages/dashboard/admin/ManageBookings.jsx
+++ foodi-client/src/pages/dashboard/admin/ManageBookings.jsx
@@ -16,7 +16,7 @@ const ManageBookings = () => {
     enabled: !loading,
     queryFn: async () => {
       const res = await fetch(
-        `http://localhost:5000/payments/all`,
+        `https://foodi-2.onrender.com/payments/all`,
         {
           headers: {
             authorization: `Bearer ${token}`,
