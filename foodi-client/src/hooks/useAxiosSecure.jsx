24155aa3b91fadb1d60918853c2fc8d0171d7bf9
Mayank Gupta <108197241+Mayank561@users.noreply.github.com>
2024-05-02 18:02:53 +0530

    Update useAxiosSecure.jsx

--- foodi-client/src/hooks/useAxiosSecure.jsx
+++ foodi-client/src/hooks/useAxiosSecure.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import useAuth from "./useAuth";
 
 const axiosSecure = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://foodi-2.onrender.com/'
 })
 
 const useAxiosSecure = () => {
